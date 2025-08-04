1297488bec617b9f122eaf582ba5642bac32a0d2
Elliot One <ElliotOneOfficial@gmail.com>
2025-08-04 22:08:43 +0100

    Refactor _08_Linq

--- _08_Linq/Employee.cs
+++ _08_Linq/Employee.cs
@@ -1,42 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08_Linq
 {
     class Employee
     {
         public int Id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public decimal salary { get; set; }
-        public int age { get; set; }
-        public List<int> appriasal { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+        public List<int> Appraisal { get; set; }
 
         public static void Display(List<Employee> employees)
         {
             foreach (var item in employees)
             {
-                Console.WriteLine($"Id={item.Id} , firstName = {item.firstName} , lastName = {item.lastName} , salary = {item.salary}");
+                Console.WriteLine(
+                    $"Id={item.Id} , " +
+                    $"firstName = {item.FirstName} , " +
+                    $"lastName = {item.LastName} , " +
+                    $"salary = {item.Salary}");
             }
         }
 
-        public static void DisplayWithApparisal(List<Employee> employees, string message)
+        public static void DisplayWithAppraisal(List<Employee> employees, string message)
         {
             if (message != string.Empty)
             {
                 Console.WriteLine(message);
                 Console.WriteLine("*************");
             }
+
             foreach (var emp in employees)
             {
-                Console.WriteLine($"Id={emp.Id} , firstName = {emp.firstName} , lastName = {emp.lastName} , salary = {emp.salary}");
-                foreach (var item in emp.appriasal)
+                Console.WriteLine($"Id={emp.Id} , " +
+                                  $"firstName = {emp.FirstName} , " +
+                                  $"lastName = {emp.LastName} , " +
+                                  $"salary = {emp.Salary}");
+
+                foreach (var item in emp.Appraisal)
                 {
                     Console.Write(item + "|");
                 }
+
                 Console.WriteLine();
             }
         }
--- _08_Linq/_02_QuerySyntax.cs
+++ _08_Linq/_02_QuerySyntax.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08_Linq
 {
@@ -13,41 +10,41 @@ namespace _08_Linq
             var filteredList = new List<Employee>();
             var listOfEmployees = new List<Employee>
             {
-                new Employee{Id = 1,firstName = "F1",lastName="L1",age = 20,salary =4000,appriasal=new List<int>{1,4,5,1,1}},
-                new Employee{Id = 2,firstName = "F2",lastName="L2",age = 21,salary =5000,appriasal=new List<int>{1,2,5,1,1}},
-                new Employee{Id = 3,firstName = "F3",lastName="L3",age = 20,salary =3000,appriasal=new List<int>{1,5,5,6,1}},
-                new Employee{Id = 4,firstName = "F4",lastName="L4",age = 30,salary =2000,appriasal=new List<int>{1,47,5,7,1}},
-                new Employee{Id = 5,firstName = "F5",lastName="L5",age = 46,salary =1000,appriasal=new List<int>{1,4,58,1,7,1}},
-                new Employee{Id = 6,firstName = "F6",lastName="L6",age = 36,salary =6000,appriasal=new List<int>{1,4,5,7,2,1}},
-                new Employee{Id = 7,firstName = "F7",lastName="L7",age = 24,salary =2000,appriasal=new List<int>{1,4,5,0,1}},
-                new Employee{Id = 8,firstName = "F8",lastName="L8",age = 51,salary =40000,appriasal=new List<int>{6,4,5,7,1}},
-                new Employee{Id = 9,firstName = "F9",lastName="L9",age = 37,salary =4100,appriasal=new List<int>{10,41,5,7,1}},
-                new Employee{Id = 10,firstName = "F10",lastName="L10",age = 19,salary =41000,appriasal=new List<int>{11,4,5,7,1}},
-                new Employee{Id = 11,firstName = "F11",lastName="L11",age = 72,salary =40700,appriasal=new List<int>{1,41,5,7,1}},
-                new Employee{Id = 12,firstName = "F12",lastName="L12",age = 60,salary =8400,appriasal=new List<int>{1,9,5,71,1}},
-                new Employee{Id = 13,firstName = "F13",lastName="L13",age = 41,salary =680000,appriasal=new List<int>{1,40,51,7,1}}
+                new Employee{Id = 1, FirstName = "F1",  LastName = "L1",  Age = 20, Salary = 4000,   Appraisal=new List<int>{1,4,5,1,1}},
+                new Employee{Id = 2, FirstName = "F2",  LastName = "L2",  Age = 21, Salary = 5000,   Appraisal=new List<int>{1,2,5,1,1}},
+                new Employee{Id = 3, FirstName = "F3",  LastName = "L3",  Age = 20, Salary = 3000,   Appraisal=new List<int>{1,5,5,6,1}},
+                new Employee{Id = 4, FirstName = "F4",  LastName = "L4",  Age = 30, Salary = 2000,   Appraisal=new List<int>{1,47,5,7,1}},
+                new Employee{Id = 5, FirstName = "F5",  LastName = "L5",  Age = 46, Salary = 1000,   Appraisal=new List<int>{1,4,58,1,7,1}},
+                new Employee{Id = 6, FirstName = "F6",  LastName = "L6",  Age = 36, Salary = 6000,   Appraisal=new List<int>{1,4,5,7,2,1}},
+                new Employee{Id = 7, FirstName = "F7",  LastName = "L7",  Age = 24, Salary = 2000,   Appraisal=new List<int>{1,4,5,0,1}},
+                new Employee{Id = 8, FirstName = "F8",  LastName = "L8",  Age = 51, Salary = 40000,  Appraisal=new List<int>{6,4,5,7,1}},
+                new Employee{Id = 9, FirstName = "F9",  LastName = "L9",  Age = 37, Salary = 4100,   Appraisal=new List<int>{10,41,5,7,1}},
+                new Employee{Id = 10,FirstName = "F10", LastName = "L10", Age = 19, Salary = 41000,  Appraisal=new List<int>{11,4,5,7,1}},
+                new Employee{Id = 11,FirstName = "F11", LastName = "L11", Age = 72, Salary = 40700,  Appraisal=new List<int>{1,41,5,7,1}},
+                new Employee{Id = 12,FirstName = "F12", LastName = "L12", Age = 60, Salary = 8400,   Appraisal=new List<int>{1,9,5,71,1}},
+                new Employee{Id = 13,FirstName = "F13", LastName = "L13", Age = 41, Salary = 680000, Appraisal=new List<int>{1,40,51,7,1}}
             };
 
-            Employee.DisplayWithApparisal(listOfEmployees, "All Employees");
+            Employee.DisplayWithAppraisal(listOfEmployees, "All Employees");
 
             filteredList = (from emp in listOfEmployees
-                           where emp.salary >= 5000 || emp.age <= 30
-                           select emp).ToList();
+                            where emp.Salary >= 5000 || emp.Age <= 30
+                            select emp).ToList();
 
-            Employee.DisplayWithApparisal(filteredList, "filtered data");
+            Employee.DisplayWithAppraisal(filteredList, "filtered data");
 
             filteredList = (from emp in listOfEmployees
-                            where emp.Id > 10 || emp.age == 20 || emp.salary >= 7000 && emp.age > 20
+                            where emp.Id > 10 || emp.Age == 20 || emp.Salary >= 7000 && emp.Age > 20
                             select emp).ToList();
 
 
             //Sorting
             filteredList = (from emp in listOfEmployees
-                            orderby emp.salary ascending
+                            orderby emp.Salary ascending
                             select emp).ToList();
 
             filteredList = (from emp in listOfEmployees
-                            orderby emp.salary ascending, emp.age
+                            orderby emp.Salary ascending, emp.Age
                             select emp).ToList();
         }
     }
